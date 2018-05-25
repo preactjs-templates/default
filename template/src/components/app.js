3a8e0464d1a71a380d38e8ea56354b816947bfb2
Jason Miller <developit@users.noreply.github.com>
2018-05-25 09:59:47 -0400

    Remove duplicate preact/debug import
    (since it's [already injected by preact-cli](https://github.com/developit/preact-cli/blob/master/src/lib/entry.js#L3-L6))

--- template/src/components/app.js
+++ template/src/components/app.js
@@ -7,10 +7,6 @@ import Profile from '../routes/profile';
 // import Home from 'async!../routes/home';
 // import Profile from 'async!../routes/profile';
 
-if (module.hot) {
-	require('preact/debug');
-}
-
 export default class App extends Component {
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
