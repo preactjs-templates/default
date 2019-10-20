86d5b9adbd181471af2db7fa51e1429194fd41b8
Marvin Hagemeister <hello@marvinh.dev>
2019-10-20 10:45:37 +0200

    Remove duplicate + unused import in tests

--- template/tests/header.test.js
+++ template/tests/header.test.js
@@ -1,9 +1,7 @@
 import { h } from 'preact';
 import Header from '../src/components/header';
-import { Link } from 'preact-router/match';
 // See: https://github.com/preactjs/enzyme-adapter-preact-pure
 import { shallow } from 'enzyme';
-import { h } from 'preact';
 
 describe('Initial Test of the Header', () => {
 	test('Header renders 3 nav items', () => {
